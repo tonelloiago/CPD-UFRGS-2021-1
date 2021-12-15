19f9e898dcee4c0e0b28f06e7fa932857766e99c
Iago Tonello <tonelloiago@gmail.com>
2021-12-14 22:57:41 -0300

    Delete nodeAlura directory

--- nodeAlura/controllers/atendimentos.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    app.get('/atendimentos', (request, response) => response.send('Servidor rodando, tudo ok'));
- 
-    app.post('/atendimentos', (request, response) => {
-        console.log(request.body);
-        response.send('Você está realizando um post');
-    })
-}
